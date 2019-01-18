2b123ab75f1d58d9c87cb03aaa1a87256c7e9bc2
Read Learn Climb <raphaelkang@gmail.com>
2019-01-18 10:29:18 -0800

    lesson-01

--- app/src/main/java/com/readlearnclimb/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/readlearnclimb/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.readlearnclimb.coderswag
+package com.readlearnclimb.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.readlearnclimb.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/readlearnclimb/coderswag/Model/Category.kt
@@ -0,0 +1,3 @@
+package com.readlearnclimb.coderswag.Model
+
+class Category(val title: String, val image: String)
