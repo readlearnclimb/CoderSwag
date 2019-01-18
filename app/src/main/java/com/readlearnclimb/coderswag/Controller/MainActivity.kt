23aac0cd16529448e6ca128812a5a0f49aa56813
Read Learn Climb <raphaelkang@gmail.com>
2019-01-18 12:30:15 -0800

    lesson-04

--- app/src/main/java/com/readlearnclimb/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/readlearnclimb/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.readlearnclimb.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.readlearnclimb.coderswag.Adapters.CategoryAdapter
 import com.readlearnclimb.coderswag.Model.Category
 import com.readlearnclimb.coderswag.R
 import com.readlearnclimb.coderswag.Services.DataService
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
