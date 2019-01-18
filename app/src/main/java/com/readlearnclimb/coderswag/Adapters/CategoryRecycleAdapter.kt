f25a266610d983b20286507c844a1e960ce07b91
Read Learn Climb <raphaelkang@gmail.com>
2019-01-18 15:24:22 -0800

    refactored for recycler view adapter

---
+++ app/src/main/java/com/readlearnclimb/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,41 @@
+package com.readlearnclimb.coderswag.Adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.readlearnclimb.coderswag.Model.Category
+import com.readlearnclimb.coderswag.R
+import kotlinx.android.synthetic.main.category_list_item.view.*
+
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder?.bindCategory(categories[position], context)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context)
+                .inflate(R.layout.category_list_item, parent, false)
+        return Holder(view)
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView?.findViewById<TextView>(R.id.categoryName)
+
+        fun bindCategory(category: Category, context: Context) {
+            val resourceId = context.resources.getIdentifier(category.image,
+                "drawable", context.packageName)
+            categoryImage?.setImageResource(resourceId)
+            categoryName?.text = category.title
+        }
+    }
+}
--- app/src/main/java/com/readlearnclimb/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/readlearnclimb/coderswag/Controller/MainActivity.kt
@@ -2,9 +2,11 @@ package com.readlearnclimb.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.readlearnclimb.coderswag.Adapters.CategoryAdapter
+import com.readlearnclimb.coderswag.Adapters.CategoryRecycleAdapter
 import com.readlearnclimb.coderswag.Model.Category
 import com.readlearnclimb.coderswag.R
 import com.readlearnclimb.coderswag.Services.DataService
@@ -12,14 +14,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
